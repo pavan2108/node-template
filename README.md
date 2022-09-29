191a74c61f03f40a2cbb6e6a8080a1dc9374a2e1
pavan2108 <pavansssai2001@gmail.com>
2022-09-29 19:08:55 +0530

    Fianlized base template

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # node-template
+
+### Template for Node and express server along with eslint and prettier
+
+## Getting Started
+
+1. Clone the repo
+2. Run `npm install`
+3. Run `npm run dev` to start the server in development mode
+4. Run `npm run start` to build the server in production mode
+
+## Formating and linting
+
+1. Run `npm run lint:fix` to fix the linting errors
+2. Run `npm run format:check` to Check if the formatting matches this Prettier’s rules by using
+3. Run `npm run format:write` to fix the formatting errors with force formatting
+4. Run `npm run lint:check` to Check if the linting matches this eslint’s rules
+
+## Features
+
+-   [x] Express server
+-   [x] Eslint
+-   [x] Prettier
+-   [x] Nodemon
+-   [x] Mongoose
+-   [x] Dotenv
+-   [x] Cors
+-   [x] jsonwebtoken
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import cors from 'cors'
 
-dotenv.config();
-const app = express();
+dotenv.config()
+const app = express()
 
-const port = process.env.PORT;
+app.use(express.json())
+app.use(cors())
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}, (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Connected to MongoDB");
+const port = process.env.PORT
+
+mongoose.connect(
+    process.env.MONGODB_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('Connected to MongoDB')
+        }
     }
-});
+)
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+    console.log(`App listening at http://localhost:${port}`)
+})
